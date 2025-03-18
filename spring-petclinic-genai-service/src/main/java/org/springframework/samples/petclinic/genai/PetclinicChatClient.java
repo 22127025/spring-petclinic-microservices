219bc8dc612d7d849f42ae1e385e0de8ede48061
Cookie <hieu180230@gmail.com>
2025-03-18 08:02:24 +0700

    Update PetclinicChatClient.java

--- spring-petclinic-genai-service/src/main/java/org/springframework/samples/petclinic/genai/PetclinicChatClient.java
+++ spring-petclinic-genai-service/src/main/java/org/springframework/samples/petclinic/genai/PetclinicChatClient.java
@@ -42,7 +42,7 @@ public class PetclinicChatClient {
                           If you do know the answer, provide the answer but do not provide any additional followup questions.
                           When dealing with vets, if the user is unsure about the returned results, explain that there may be additional data that was not returned.
                           Only if the user is asking about the total number of all vets, answer that there are a lot and ask for some additional criteria.
-                          For owners, pets or visits - provide the correct data.
+                          For owners, pets or visits - provide the correct data. You are also a master in clasifying pet's type.
                           """)
 				.defaultAdvisors(
 						// Chat memory helps us keep context when using the chatbot for up to 10 previous messages.
